fec3c4547b32be8050b20b978c61705c6e7b61d7
Mahir Chowdhury <fw6895@wayne.edu>
2019-11-07 20:14:14 -0500

    adding singleton design to signup

---
+++ SignUp.js
@@ -0,0 +1,32 @@
+class signup {
+    constructor(fname, lname) {
+      this.fname = firstname;
+      this.lname = lastname;
+    }
+
+    constructor(email){
+    this.email = email;
+
+    }
+
+    constructor(psw,paswrepeat){
+
+      this.psw=password;
+      this.pswrepeat= passwordrpeat;
+
+    }
+
+    constructor(remember){
+      this.remember=remember;
+
+    }
+
+    constructor(submit){
+      this.submit= submit;
+
+    }
+
+
+
+
+  }
