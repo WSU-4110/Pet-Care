3b208db74028cfe3c23041c6d9b4634ef3ba1f6a
Santhosh Abraham <fv3517@wayne.edu>
2019-11-07 19:45:49 -0500

    added singleton design to login page

---
+++ main.js
@@ -0,0 +1,9 @@
+// This is for sprint 3 and database. Still a work in progess,
+// but this is what my singleton class will look like.
+class User {
+    constructor(username,password,email){
+        this.username = username;
+        this.password = password;
+        this.email=email;
+    }
+}
