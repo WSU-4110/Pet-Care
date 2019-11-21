45b98860d4b46355f16f4024b58eba7ffdca8c95
Santhosh <fv3517@wayne.edu>
2019-11-21 02:17:33 -0500

    adding home route

--- nodejs-mongodb/routes/index.js
+++ nodejs-mongodb/routes/index.js
@@ -5,3 +5,8 @@ var User      = mongoose.model('Users');
 var crypto    = require('crypto'), hmac, signature;
 const { check, validationResult } = require('express-validator/check');
 const { matchedData, sanitize }   = require('express-validator/filter');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Petcare Sign-Up'});
+ })
