fafc43671102b4aeed8b16346a7576ed0b3d6453
Santhosh <fv3517@wayne.edu>
2019-11-21 02:00:04 -0500

    adding more files

---
+++ nodejs-mongodb/models/User.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+var userSchema = new Schema({
+
+  
