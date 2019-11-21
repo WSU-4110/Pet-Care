70ed261883e1ee32ad70cd83d878ab52fcb38b63
Omar Faruq <fy4493@wayne.edu>
2019-11-21 02:46:51 -0500

    Connection to the database

--- ListingDB/app.js
+++ ListingDB/app.js
@@ -2,3 +2,6 @@ var express     = require("express");
 var app         = express();
 var bodyParser  = require("body-parser");
 var mongoose    = require("mongoose");
+
+//database connection
+mongoose.connect("mongodb://localhost/LostAndFound_Listing");
