8188daccf4dc2e0ffc04d82da9a0858aada28c1e
Santhosh <fv3517@wayne.edu>
2019-11-21 02:01:37 -0500

    code

--- nodejs-mongodb/models/User.js
+++ nodejs-mongodb/models/User.js
@@ -9,4 +9,15 @@ var validateEmail = function(email) {
 
 var userSchema = new Schema({
 
+    full_name: { type: String,  required: [true, 'Full name must be provided'] },
+
+    email:    { 
+      
+      type: String,     
   
+      Required:  'Email address cannot be left blank.',
+      validate: [validateEmail, 'Please fill a valid email address'],
+           match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+      index: {unique: true, dropDups: true}
+      },
+      
