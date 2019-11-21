ffabf8fb2bdaa36b0126b8ac115eb75113c56561
Santhosh <fv3517@wayne.edu>
2019-11-21 02:16:17 -0500

    adding routes folder

---
+++ nodejs-mongodb/routes/index.js
@@ -0,0 +1,7 @@
+var express   = require('express');
+var router    = express.Router();
+var mongoose  = require('mongoose');
+var User      = mongoose.model('Users');
+var crypto    = require('crypto'), hmac, signature;
+const { check, validationResult } = require('express-validator/check');
+const { matchedData, sanitize }   = require('express-validator/filter');
