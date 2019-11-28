19811eabe4ff985dd9678a01557e2a2bbf76d249
Mahir Chowdhury <mahir.chowdhury2@wayne.edu>
2019-11-28 14:52:07 -0500

    Added Java script test file for assignment 6

---
+++ functions.js
@@ -0,0 +1,14 @@
+// This will create the first name for the user while they give infromation on the stray animal page  
+
+const functions = {
+
+      createuser: () => {
+
+        const user = { firstname:'mahir' }
+          user['lastname'] = 'chowdhury';
+          return user;
+
+    }    
+};
+
+    module.exports= functions;
