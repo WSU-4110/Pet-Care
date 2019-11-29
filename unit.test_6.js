02c808455c828a552c942e331788f526cc1a1393
Mahir Chowdhury <mahir.chowdhury2@wayne.edu>
2019-11-28 21:49:47 -0500

    UNIT TEST 6

---
+++ unit.test_6.js
@@ -0,0 +1,9 @@
+// This will fetch the username from the databse according to the profile while logged it
+
+// Async Await
+test('User fetched name should be Leanne Graham', async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual('Leanne Graham');
+});
+
